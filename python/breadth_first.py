a90f7aba043a2a96c8526fb0b9c26dc7f6e4fbbc
Arafan <a.m.arafan@students.uu.nl>
2023-02-21 01:07:48 +0100

    more breadth

--- python/breadth_first.py
+++ python/breadth_first.py
@@ -3,10 +3,34 @@ from collections import deque
 def person_is_seller(name: str) -> bool:
     return name[-1]  == 'm'
 
-def mongo_seller():
-    pass
+def mongo_seller(search_queue, graph, searched):
+    if search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_is_seller(person):
+                print("{} is a mongo seller!".format(person))
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+                return mongo_seller(search_queue, graph, searched)
+    return False
 
 
 if __name__ == "__main__":
     graph = {}
+    graph["you"] = ["alice", "bobm", "claire"]
+    graph["bob"] = ["anuj", "peggy"]
+    graph["alice"] = ["peggy"]
+    graph["claire"] = ["thom", "jonny"]
+    graph["anuj"] = []
+    graph["peggym"] = []
+    graph["thom"] = []
+    graph["jonny"] = []
+    graph["anuj"] = []
+    graph["claire"] = ["thom", "jonny"]
+
     search_queue = deque()
+    search_queue += graph["you"]
+    mongo_seller(search_queue=search_queue, 
+        graph=graph, searched=[])
